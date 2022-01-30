18fb43727cbc6b47fd58b087410243cc216aebf2
Dennis Flæng <dennis.flaeng@gmail.com>
2022-01-30 15:05:27 +0100

    Commented code out and renamed file in test folder

--- tests-with-errors/github-02-no_code.js
+++ tests-with-errors/github-02-no_code.js
@@ -1,5 +1,5 @@
 const puppeteer = require('puppeteer');
-
+/*
 (async () => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
@@ -170,3 +170,4 @@ const puppeteer = require('puppeteer');
 
     await browser.close();
 })();
+*/
