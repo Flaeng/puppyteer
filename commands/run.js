3995a20570d61a73d57295d7a9f5f3e6e6b579d7
Dennis Flæng <dennis.flaeng@gmail.com>
2022-01-31 10:05:52 +0100

    Added domain option and duration on successfull tests

--- commands/run.js
+++ commands/run.js
@@ -12,12 +12,14 @@ async function runScriptAsync(path) {
     try {
         return await new Promise((resolve, reject) => {
             const cmd = `node ${path}`;
+            const start = new Date();
             exec(cmd, (err, stdout, stderr) => {
+                const duration = new Date() - start;
                 if (err) {
-                    reject({ exitCode: 1, error: err });
+                    reject({ exitCode: 1, error: err, duration });
                     return;
                 }
-                resolve({ exitCode: 0, error: null });
+                resolve({ exitCode: 0, error: null, duration });
             })
         });
     }
@@ -35,13 +37,14 @@ async function runAllScriptsAsync(testList, showErrors) {
                 spinner: cliSpinners.point,
                 prefixText: `[${(index + 1).toString().padStart(testList.length.toString().length, '0')}/${testList.length}] ${elem.filename}`
             }).start();
-            
+
             const result = await runScriptAsync(elem.filepath);
-            
+
             if (result.exitCode === 0) {
-                spinner.succeed(chalk.green.bold('success'));
+                spinner.succeed(chalk.green.bold(`success`) + ` in ${result.duration}ms`);
             } else {
-                spinner.fail(chalk.red.bold('failed'));
+                //spinner.fail(chalk.red.bold(`failed in ${result.duration}ms`));
+                spinner.fail(chalk.red.bold(`failed`));
                 if (showErrors === true) {
                     console.error(result.error);
                 }
@@ -56,19 +59,85 @@ async function runAllScriptsAsync(testList, showErrors) {
     return testList.filter(x => x.exitCode === 0).length === testList.length;
 }
 
+async function handleDomain(tests, domain) {
+    return await new Promise(async (resolve, reject) => {
+        const tempFolder = `./.puppyteer/`;
+        const dom = (domain.startsWith('http://') || domain.startsWith('https://')) ? domain : `https://${domain}`;
+        for (let index = 0; index < tests.length; index++) {
+            const element = tests[index];
+
+            await new Promise((fileWriteResolve, reject) => {
+                fs.readFile(element.filepath, 'utf8', (err, data) => {
+                    if (err) {
+                        console.warn(err);
+                        return;
+                    }
+
+                    const dataSplit = data.split('\n');
+                    const targetPageGoToIndex = dataSplit.map((x, index) => {
+                        if (x.trimStart().startsWith('await targetPage.goto(')) {
+                            return index;
+                        } else {
+                            return -1;
+                        }
+                    }).filter(x => x != -1);
+                    if (targetPageGoToIndex.length === 0) {
+                        throw `Failed to find line with "await targetPage.goto(" in file at path: ${element.filepath}`;
+                    }
+                    dataSplit[targetPageGoToIndex[0]] = `        await targetPage.goto('${dom}');`;
+
+                    const newFilePath = `${tempFolder}${element.filepath}`;
+                    const directoryPath = newFilePath.split('/').slice(0, -1).join('/');
+                    if (!fs.existsSync(directoryPath)) {
+                        fs.mkdirSync(directoryPath, { recursive: true });
+                    }
+
+                    fs.writeFile(newFilePath, dataSplit.join('\n'), err => {
+                        if (err) {
+                            console.warn(err);
+                            return;
+                        }
+                        console.log('newFilePath', newFilePath);
+                        element.filepath = newFilePath;
+                        fileWriteResolve();
+                    })
+                });
+            });
+        }
+        resolve();
+    });
+}
+
 async function run(args) {
     const relativePath = args.path || process.cwd();
 
+    //make sure path exists
     if (fs.existsSync(relativePath) === false) {
         console.error(`Directory at path not found: ${relativePath}`);
         return 1;
     }
 
+    //get all tests based on filter and file extension
     var tests = glob.sync(path.join(relativePath, args.filter || "*"))
         .filter(x => path.extname(x).toLocaleLowerCase() === '.js')
         .map(x => { return { filepath: x, filename: path.basename(x), exitCode: null, error: null }; });
 
+    //if domain is set, rewrite tests
+    if (args.domain) {
+        await handleDomain(tests, args.domain);
+    }
+
+    //execute
     const didSucceed = await runAllScriptsAsync(tests, args.errors || false);
+    
+    //cleanup
+    if (args.domain) {
+        await new Promise((resolve, reject) => {
+            fs.rmSync('./.puppyteer', { recursive: true, force: true }, resolve);
+        });
+    }
+
+    //handle result
     if (didSucceed === false) {
         if (core) {
             core.setFailed('One or more scripts failed');
--- index.js
+++ index.js
@@ -7,6 +7,7 @@ program
     .option('-p, --path <path>', 'Path to files (default: current directory)')
     .option('-f, --filter <filter>', 'Filter which files should be runned (default: "*")')
     .option('-e, --errors', 'Show detailed error messages')
+    .option('-d, --domain <domain>', 'Change the domain of the url that all tests starts on')
     .description('Runs all .js files in selected path')
     .action(run);
 
